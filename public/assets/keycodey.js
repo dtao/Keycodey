ed877e1d371b3c62a964a7e62ff6ed1a18026531
Dan Tao <daniel.tao@gmail.com>
2012-11-06 17:06:00 -0800

    added option to keep codes on screen

--- public/assets/keycodey.js
+++ public/assets/keycodey.js
@@ -1,10 +1,60 @@
-var keyCharElement = document.getElementById("keychar");
-var keyCodeElement = document.getElementById("keycode");
+var screenWrapper             = $("#screen-wrapper");
+var keyCharElement            = $("#keychar");
+var keyCodeElement            = $("#keycode");
+var keepCodesOnScreenCheckbox = $("input#keep-codes-on-screen");
+var clearLink                 = $("a.clear");
 
-document.body.onkeypress = function(e) {
-  keyCharElement.innerText = "Char: " + e.charCode;
-};
+var currentCodes = {};
 
-document.body.onkeydown = function(e) {
-  keyCodeElement.innerText = "Code: " + e.keyCode;
-};
+function keepCodesOnScreen() {
+  return keepCodesOnScreenCheckbox.is(":checked");
+}
+
+function addToScreenIfReady() {
+  if (currentCodes.charCode && currentCodes.keyCode) {
+    var codeElement = $("<div>").addClass("codes");
+    $("<div>").addClass("char-code").text("Char: " + currentCodes.charCode).appendTo(codeElement);
+    $("<div>").addClass("key-code").text("Key: " + currentCodes.keyCode).appendTo(codeElement);
+    codeElement.appendTo(screenWrapper);
+    currentCodes = {};
+  }
+}
+
+function addChar(char) {
+  currentCodes.char = char;
+}
+
+function addCharCode(code) {
+  currentCodes.charCode = code;
+  addToScreenIfReady();
+}
+
+function addKeyCode(code) {
+  currentCodes.keyCode = code;
+  addToScreenIfReady();
+}
+
+function clear() {
+  screenWrapper.empty();
+  keyCharElement.empty();
+  keyCodeElement.empty();
+}
+
+$("body").keypress(function(e) {
+  if (keepCodesOnScreen()) {
+    addCharCode(e.charCode);
+  } else {
+    keyCharElement.text("Char: " + e.charCode);
+  }
+});
+
+$("body").keydown(function(e) {
+  if (keepCodesOnScreen()) {
+    addKeyCode(e.keyCode);
+  } else {
+    keyCodeElement.text("Code: " + e.keyCode);
+  }
+});
+
+keepCodesOnScreenCheckbox.change(clear);
+clearLink.click(clear);
